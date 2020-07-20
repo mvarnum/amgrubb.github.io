abf35012692eef8f0828bee2ca33f70922c2a0e7
Kate Spencer <47483484+kmbspencer@users.noreply.github.com>
2020-07-20 11:20:27 -0700

    publications and python

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -36,6 +36,7 @@ import pandas as pd
 
 publications = pd.read_csv("publications.tsv", sep="\t", header=0)
 publications
+#print("publications: " +publications+" end publications.")
 
 
 # ## Escape special characters
@@ -63,9 +64,12 @@ def html_escape(text):
 
 import os
 for row, item in publications.iterrows():
-    
-    md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
-    html_filename = str(item.pub_date) + "-" + item.url_slug
+    str_pub_date = str(item.pub_date)
+    str_url_slug = str(item.url_slug)
+    #print("url slug: "+str_url_slug)
+    #print("pub date from item, and in string form: "+item.pub_date + " " + str_pub_date)
+    md_filename = str_pub_date + "-" + str_url_slug + ".md"
+    html_filename = str_pub_date + "-" + str_url_slug
     year = item.pub_date[:4]
     
     ## YAML variables
